45a727de36aa6f833a27de169adaa3d665941093
akshataraichurkar <akshataraichurkar06@gmail.com>
2026-08-01 22:24:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function openGift() {
+
+    document.getElementById("welcome").style.display = "none";
+    document.getElementById("main").style.display = "block";
+
+}
+
+function showMessage() {
+
+    document.getElementById("message").innerHTML =
+    `💖 Happy Friendship Day! 💖<br><br>
+    Dear Bestie,<br><br>
+
+    Thank you for being such an amazing friend. ❤️<br><br>
+
+    Every laugh, every memory, every conversation and every moment with you is truly special to me. 🌸<br><br>
+
+    No matter where life takes us, I hope we always stay friends forever. 🫂💕<br><br>
+
+    Wishing you lots of happiness, success, good health and endless smiles. ✨<br><br>
+
+    Happy Friendship Day! 🌷❤️`;
+}
